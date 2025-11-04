b9dfc8b1901315809cf921290962331bc0d679a4
Narancia34 <tahagamraoui@gmail.com>
2025-11-04 18:45:04 +0100

    harl 2.0 finished

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,10 +6,44 @@
 /*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 16:57:04 by mgamraou          #+#    #+#             */
-/*   Updated: 2025/11/04 16:57:12 by mgamraou         ###   ########.fr       */
+/*   Updated: 2025/11/04 17:29:53 by mgamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+void Harl::debug(void){
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-"
+		 "triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
 
+void Harl::info(void){
+	std::cout << "I cannot believe adding extra bacon costs more money."
+	" You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+	<< std::endl;
+}
+
+void Harl::warning(void){
+	std::cout << "I think I deserve to have some extra bacon for free."
+	"I’ve been coming for years, whereas you started working here just last month."
+	<< std::endl;
+}
+
+void Harl::error(void){
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level){
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	void (Harl::*fcnPtr[4])(void) = {&Harl::debug,
+					&Harl::info,
+					&Harl::warning,
+					&Harl::error};
+	for (int i = 0;i < 4; i++){
+		if (levels[i] == level){
+			(this->*fcnPtr[i])();
+			return;
+		}
+	}
+	std::cout << "i am skibidi" <<  std::endl;
+}
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -10,4 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
 
+int main(){
+	Harl harl;
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("DEBUG");
+	harl.complain("ERROR");
+	harl.complain("hello?");
+}
