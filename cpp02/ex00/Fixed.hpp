0a22985f744d9f6b765c483fedcc30579b68e905
Narancia34 <tahagamraoui@gmail.com>
2025-11-14 13:57:12 +0100

    init Fixed class

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 14:49:42 by mgamraou          #+#    #+#             */
+/*   Updated: 2025/11/12 16:14:38 by mgamraou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <stdio.h>
+
+class Fixed{
+private:
+	int _value;
+	static const int _fractionalBits;
+public:
+	Fixed();
+	Fixed(const Fixed& other);
+	Fixed& operator=(const Fixed& other);
+	~Fixed();
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+};
+
+#endif
