6cbaaad2bea90c3e1934f4c217f0ef77efdb9ae2
Narancia34 <tahagamraoui@gmail.com>
2025-11-17 19:15:01 +0100

    init Point class in orthodox canonical form

--- cpp02/ex03/Point.cpp
+++ cpp02/ex03/Point.cpp
@@ -6,7 +6,21 @@
 /*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 18:21:36 by mgamraou          #+#    #+#             */
-/*   Updated: 2025/11/17 18:21:38 by mgamraou         ###   ########.fr       */
+/*   Updated: 2025/11/17 19:14:26 by mgamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Point.hpp"
+
+Point::Point():x(),y(){}
+
+Point::Point(const float x, const float y):x(x),y(y){}
+
+Point::Point(const Point& other):x(other.x), y(other.y) {}
+
+Point& Point::operator=(const Point& other){
+	(void)other;
+	return *this;
+}
+
+Point::~Point(){}
--- cpp02/ex03/Point.hpp
+++ cpp02/ex03/Point.hpp
@@ -6,7 +6,27 @@
 /*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 18:21:42 by mgamraou          #+#    #+#             */
-/*   Updated: 2025/11/17 18:21:45 by mgamraou         ###   ########.fr       */
+/*   Updated: 2025/11/17 19:13:19 by mgamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include "Fixed.hpp"
+
+class Point{
+private:
+	Fixed const x;
+	Fixed const y;
+public:
+	Point();
+	Point(const float x, const float y);
+	Point(const Point& other);
+	Point& operator=(const Point& other);
+	~Point();
+};
+
+bool bsp( Point const a, Point const b, Point const c, Point const point);
+
+#endif
