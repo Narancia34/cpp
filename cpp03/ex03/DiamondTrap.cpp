4ef9e58566b49591b908f9937461b595fc3dceb8
Narancia34 <tahagamraoui@gmail.com>
2025-12-03 18:34:09 +0100

    feat(wip): implementing DiamondTrap class

--- cpp03/ex03/DiamondTrap.cpp
+++ cpp03/ex03/DiamondTrap.cpp
@@ -6,18 +6,39 @@
 /*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 16:30:46 by mgamraou          #+#    #+#             */
-/*   Updated: 2025/12/03 16:31:33 by mgamraou         ###   ########.fr       */
+/*   Updated: 2025/12/03 18:33:36 by mgamraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
+#include "ClapTrap.hpp"
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
 
-DiamondTrap::DiamondTrap(){}
+DiamondTrap::DiamondTrap() : ClapTrap("", 100, 50, 30){
+	std::cout << "DiamondTrap constructor called" << std::endl;
+}
+DiamondTrap::DiamondTrap(std::string name):
+	ClapTrap(name + "_clap_name", 100, 50, 30),
+	ScavTrap(name), FragTrap(name){
+	this->_name = name;
+	std::cout << "DiamondTrap parameterized constructor called" << std::endl;
+}
 
-DiamondTrap::DiamondTrap(const DiamondTrap &other){}
+DiamondTrap::DiamondTrap(const DiamondTrap &other): ClapTrap(other), ScavTrap(other), FragTrap(other){
+	this->_name = other._name;
+	std::cout << "DiamondTrap copy constructor called" << std::endl;
+}
 
 DiamondTrap &DiamondTrap::operator=(const DiamondTrap &other){
+	if (this != &other){
+		ClapTrap::operator=(other);
+		this->_name = other._name;
+	}
+	std::cout << "DiamondTrap copy assignment operator called" << std::endl;
         return (*this);
 }
 
-DiamondTrap::~DiamondTrap(){}
+DiamondTrap::~DiamondTrap(){
+	std::cout << "DiamondTrap destructor called" << std::endl;
+}
