79f79619f93a13ca0964a7e14cccc81be7a2c4a0
Narancia34 <tahagamraoui@gmail.com>
2025-12-22 19:27:00 +0100

    feat: impelement Brain class

---
+++ cpp04/ex01/Brain.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/22 19:04:13 by mgamraou          #+#    #+#             */
+/*   Updated: 2025/12/22 19:20:40 by mgamraou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BRAIN_HPP
+#define BRAIN_HPP
+
+#include <iostream>
+
+class Brain{
+private:
+	std::string _ideas[100];
+public:
+	Brain();
+	Brain(std::string ideas[]);
+	Brain(const Brain &other);
+	Brain &operator=(const Brain &other);
+	~Brain();
+};
+
+#endif
