536402ae3bbbbdad33c1074cb123118e94a62c45
Narancia34 <tahagamraoui@gmail.com>
2026-04-03 23:27:44 +0100

    feat: implemented methods to handle pseudo literals and char literals

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: nara <nara@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 18:10:19 by mgamraou          #+#    #+#             */
-/*   Updated: 2026/04/02 18:13:32 by mgamraou         ###   ########.fr       */
+/*   Updated: 2026/04/03 23:26:54 by nara             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,46 @@ ScalarConverter::ScalarConverter(const ScalarConverter &other){ (void)other;}
 
 ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other){(void)other;}
 
+bool ScalarConverter::isPseudoLiteral(const std::string &literal) {
+    return (literal == "nan" || literal == "nanf" ||
+            literal == "+inf" || literal == "+inff" ||
+            literal == "-inf" || literal == "-inff");
+}
 
+void ScalarConverter::convertPseudoLiteral(const std::string &literal){
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    if (literal == "nanf" || literal == "+inff" || literal == "-inff"){
+        std::cout << "float: " << literal << std::endl;
+        std::cout << "double: " << literal.substr(0, literal.length() - 1) << std::endl;
+    }
+    else{
+        std::cout << "float: " << literal << "f" << std::endl;
+        std::cout << "double: " << literal << std::endl;
+    }
+}
+
+bool ScalarConverter::isChar(const std::string &literal) {
+    return (literal.length() == 1 && !std::isdigit(literal[0]));
+}
+
+void ScalarConverter::convertChar(const std::string &literal){
+    char c = literal[0];
+    std::cout << "char: '" << c << "'" << std::endl;
+    std::cout << "int: " << static_cast<int>(c) << std::endl;
+    std::cout << "float: " << static_cast<float>(c) << std::endl;
+    std::cout << "double: " << static_cast<double>(c) << std::endl;
+}
+
+bool ScalarConverter::isInt(const std::string &literal) {
+    int i = 0;
+    int j = literal.length();
+    if (literal[0] == '-' || literal[0] == '+')
+        i++;
+    while (i < j) {
+        if (!std::isdigit(literal[i]))
+            return false;
+        i++;
+    }
+    return true;
+}
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgamraou <mgamraou@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: nara <nara@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 20:30:43 by mgamraou          #+#    #+#             */
-/*   Updated: 2026/04/01 20:42:33 by mgamraou         ###   ########.fr       */
+/*   Updated: 2026/04/03 23:19:00 by nara             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,16 @@ private:
 	ScalarConverter(const ScalarConverter &other);
 	ScalarConverter &operator=(const ScalarConverter &other);
 	~ScalarConverter();
+	static bool isPseudoLiteral(const std::string &literal);
+	static bool isChar(const std::string &literal);
+	static bool isInt(const std::string &literal);
+	static bool isFloat(const std::string &literal);
+	static bool isDouble(const std::string &literal);
+	static void convertPseudoLiteral(const std::string &literal);
+	static void convertChar(const std::string &literal);
+	static void convertInt(const std::string &literal);
+	static void convertFloat(const std::string &literal);
+	static void convertDouble(const std::string &literal);
 public:
 	static void convert(const std::string &literal);
 };
